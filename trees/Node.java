5d4dfa6a121d11462c0ed4a44f8e90cf11b52117
ravi maddi <ravimaddi@ravis-MacBook-Air.local>
2022-01-19 16:16:41 -0700

    treesday2

--- trees/Node.java
+++ trees/Node.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Node {
   int key;
   Node left, right;
@@ -57,7 +60,112 @@ class BinaryTree {
     }
     return 1 + size(node.left) + size(node.right);
   }
+   
+  public void printKNode(Node node,int k) {
+    if (node == null) {
+      return;
+    }
+    if (k == 0) {
+      System.out.print(node.key + " ");
+    } else {
+      printKNode(node.left, k - 1);
+      printKNode(node.right, k-1);
+    }
 
+  }
+
+  public void levelOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+    Queue<Node> q = new LinkedList<>();
+    q.add(node);
+    q.add(null);
+    while (q.size() > 1) {
+      Node curr = q.poll();
+      if (curr != null) {
+        System.out.print(curr.key + " ");
+      } else {
+        System.out.print("\n");
+        q.add(null);
+        continue;
+      }
+      if (curr.left != null) {
+        q.add(curr.left);
+      }
+      if (curr.right != null) {
+        q.add(curr.right);
+      }
+    }
+  }
+  
+  public void levelOrderLine(Node node) {
+    if (node == null) {
+      return;
+    }
+    Queue<Node> q = new LinkedList<>();
+    q.add(node);
+    while (!q.isEmpty()) {
+      int s = q.size();
+      for (int i = 0; i < s; i++) {
+        Node curr = q.poll();
+        System.out.print(curr.key + " ");
+        if (curr.left != null) {
+          q.add(curr.left);
+        }
+        if (curr.right != null) {
+          q.add(curr.right);
+        }
+      }
+      System.out.print("\n");
+    }
+  }
+  
+  public void leftView(Node node) {
+    if (node == null) {
+      return;
+    }
+    Queue<Node> q = new LinkedList<>();
+    q.add(node);
+    while (!q.isEmpty()) {
+      int s = q.size();
+      for (int i = 0; i < s; i++) {
+        Node curr = q.poll();
+        if (i == 0) {
+          System.out.print(curr.key + " ");
+        }
+        if (curr.left != null) {
+          q.add(curr.left);
+        }
+        if (curr.right != null) {
+          q.add(curr.right);
+        }
+      }
+      System.out.print("\n");
+    }
+  }
+
+  static int maxLevel = 0;
+  public void helper(Node node, int level) {
+    if (node == null) {
+      return;
+    }
+    if (maxLevel < level) {
+      System.out.println(node.key);
+      maxLevel = level;
+    }
+    helper(node.left, level + 1);
+    helper(node.right, level+1);
+  }
+  public void leftViewRecu(Node node) {
+    helper(node, 1);
+  }
+  public int maximumNode(Node node) {
+    if (node == null) {
+      return Integer.MIN_VALUE;
+    }
+    return Math.max(node.key, Math.max(maximumNode(node.left), maximumNode(node.right)));
+  }
   public static void main(String[] args) {
     BinaryTree tree = new BinaryTree();
     tree.root = new Node(10);
@@ -76,5 +184,17 @@ class BinaryTree {
     System.out.println(tree.height(tree.root));
     System.out.println("\n");
     System.out.println(tree.size(tree.root));
+    System.out.println("\n");
+    tree.printKNode(tree.root, 2);
+    System.out.println("\n");
+    tree.levelOrder(tree.root);
+    System.out.println("\n");
+    tree.levelOrderLine(tree.root);
+    System.out.println("\n");
+    System.out.println(tree.maximumNode(tree.root));
+    System.out.println("\n");
+    tree.leftView(tree.root);
+    System.out.println("\n");
+    tree.leftViewRecu(tree.root);
   }
 }
